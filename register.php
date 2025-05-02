d075790041884eb09599c1247650c7a623111335
rofusedcryof <dezttroll@gmail.com>
2025-05-02 11:09:33 +0700

    menambah class untuk registrasi

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php include "config.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="login-box">
+        <h2>Register</h2>
+        <form action="proses_register.php" method="post">
+            <label>Username:</label><br>
+            <input type="text" name="username" required><br>
+            <label>Password:</label><br>
+            <input type="password" name="password" required><br><br>
+            <button type="submit">Daftar</button>
+        </form>
+        <p style="text-align:center;">Sudah punya akun? <a href="index.php">Login</a></p>
+    </div>
+</body>
+</html>

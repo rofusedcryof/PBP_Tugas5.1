abf83ec340003f1260cf3cbaa7183c743296b9df
rofusedcryof <dezttroll@gmail.com>
2025-05-08 18:55:56 +0700

    menambah class proses login

--- proses_login.php
+++ proses_login.php
@@ -2,7 +2,7 @@
 include "config.php";
 
 $username = $_POST['username'];
-$password = hash('sha256', $_POST['password']); // Gunakan hash SHA-256
+$password = hash('sha256', $_POST['password']);
 
 $sqpl = "SELECT * FROM users WHERE username=? AND password=?";
 $stmt = $conn->prepare($sqpl);
@@ -10,12 +10,12 @@ $stmt->bind_param("ss", $username, $password);
 $stmt->execute();
 $result = $stmt->get_result();
 
-if ($result->num_rows == 1) {  // Gunakan perbandingan ==
+if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
-    $_SESSION['user_id'] = $row['id'];         // Gunakan $row, bukan $user
+    $_SESSION['user_id'] = $row['id'];
     $_SESSION['username'] = $row['username'];
     header("Location: todo.php");
-    exit(); // Tambahkan exit agar script berhenti setelah redirect
+    exit();
 } else {
     echo "<script>alert('Username atau Password salah!');window.location='index.php';</script>";
 }

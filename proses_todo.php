85656da8efba7bb1a3de7bb6046d6f05e6c4fb2a
rofusedcryof <dezttroll@gmail.com>
2025-05-08 15:19:43 +0700

    memperbaiki class proses todo

--- proses_todo.php
+++ proses_todo.php
@@ -1,10 +1,9 @@
 <?php
 include "config.php";
-include "functions.php";  // Pastikan functions.php di-*include* untuk fungsi redirectIfNotLoggedIn()
+include "functions.php";
 redirectIfNotLoggedIn();
 $user_id = $_SESSION['user_id'];
 
-// Menambahkan task baru
 if (isset($_POST['add'])) {
     $task = trim($_POST['task']);
     if ($task != "") {
@@ -14,24 +13,19 @@ if (isset($_POST['add'])) {
     }
 }
 
-// Menandai tugas sebagai selesai
 if (isset($_GET['done'])) {
     $id = intval($_GET['done']);
-    // Gunakan prepared statement untuk keamanan
     $stmt = $conn->prepare("UPDATE todos SET is_done = 1 WHERE id = ? AND user_id = ?");
     $stmt->bind_param("ii", $id, $user_id);
     $stmt->execute();
 }
 
-// Menghapus task
 if (isset($_GET['delete'])) {
     $id = intval($_GET['delete']);
-    // Gunakan prepared statement untuk keamanan
     $stmt = $conn->prepare("DELETE FROM todos WHERE id = ? AND user_id = ?");
     $stmt->bind_param("ii", $id, $user_id);
     $stmt->execute();
 }
 
-// Redirect ke halaman todo.php setelah aksi selesai
 header("Location: todo.php");
 exit();

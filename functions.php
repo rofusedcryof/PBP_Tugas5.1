1d919633211e87910aeed0a408444850fc18a82f
rofusedcryof <dezttroll@gmail.com>
2025-05-08 15:09:50 +0700

    memperbaiki kelas functions

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+function isLoggedIn(){
+    return isset($_SESSION['user_id']);
+}
+
+function redirectIfNotLoggedIn(){
+    if (!isLoggedIn()) {
+        header("Location: index.php");
+        exit();
+    }
+}
+?>

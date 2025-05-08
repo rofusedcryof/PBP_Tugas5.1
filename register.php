c5ad5655724bb340baa2199391fd0f6090fafc7c
rofusedcryof <dezttroll@gmail.com>
2025-05-08 18:41:41 +0700

    menambah placeholder untuk memperjelas teks yg harus diinput

--- register.php
+++ register.php
@@ -10,9 +10,9 @@
         <h2>Register</h2>
         <form action="proses_register.php" method="post">
             <label>Username:</label><br>
-            <input type="text" name="username" required><br>
+            <input type="text" name="username" placeholder="Masukkan Nama" required><br>
             <label>Password:</label><br>
-            <input type="password" name="password" required><br><br>
+            <input type="password" name="password" placeholder= "Masukkan Password yg unik" required><br><br>
             <button type="submit">Daftar</button>
         </form>
         <p style="text-align:center;">Sudah punya akun? <a href="index.php">Login</a></p>

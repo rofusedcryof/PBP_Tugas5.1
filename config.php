3087181a1fca1b6122d5cbe20b35f8626a3b4d82
rofusedcryof <dezttroll@gmail.com>
2025-05-08 11:13:52 +0700

    mengubah koneksi

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 $host = "localhost";
-$username = "Admin";
+$username = "root";
 $db = "todo_app";
 $password = "12345678";
 
 $conn = mysqli_connect($host, $username, $password, $db);
-if ($conn->connect_error) {
+if (mysqli_connect_error()) {
     die("Connection failed: " . mysqli_connect_error());
 }
 session_start();

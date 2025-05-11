2f572dd877562d6fb94f442a9c9359c90eafa7f4
rofusedcryof <dezttroll@gmail.com>
2025-05-11 13:54:33 +0700

    mengubah prompt include menjadi required_once

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
 <?php
-session_start();
+// session_start();
+// session_destroy();
+// header("Location: index.php");
+// exit();
+require_once "config.php";
 session_destroy();
 header("Location: index.php");
 exit();

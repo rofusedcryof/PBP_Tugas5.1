d41f327b2e4121caf9412e1c2698c7674e84e7b0
rofusedcryof <dezttroll@gmail.com>
2025-05-11 13:51:52 +0700

    mengubah pemanggilan session start menjadi satu kali

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 $host = "localhost";
 $username = "root";
 $db = "todo_app";
@@ -8,5 +12,5 @@ $conn = mysqli_connect($host, $username, $password, $db);
 if (mysqli_connect_error()) {
     die("Connection failed: " . mysqli_connect_error());
 }
-session_start();
+// session_start();
 ?>
